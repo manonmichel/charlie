b5880d1d73466d72533a79fe94d50ba5d99f6796
manonmichel <manon.michel.iphone@gmail.com>
2017-10-25 23:17:58 +0200

    Add files via upload

--- MIniproject_test1.java
+++ MIniproject_test1.java
@@ -6,24 +6,28 @@ public class MIniproject_test1 {
 	   }
 	    public static void testGetRed() { 
 			int color = 0b11110000_00001111_01010101;
+			System.out.println(Integer.toBinaryString(color));
 			int ref = 0b11110000;
 			int red = getRed(color);
-			//if (red == ref) {
-			//	System.out.println("Test passed");
-			//} else {
-			//	System.out.println("Test failed. Returned value = " + red + " Expected value = " + ref);
-			//}
+			if (red == ref) {
+				System.out.println("Test passed");
+			} else {
+				System.out.println("Test failed. Returned value = " + red + " Expected value = " + ref);
+			}
 	    }
 	    public static int getRed(int rgb) {
-			String sRGB = "" + rgb;
+			String sRGB = Integer.toBinaryString(rgb);
 			int red = 0; 
-			System.out.print(sRGB);
-			//for (int i=2; i<10; i++) {
-				//int a = (int) sRGB.charAt(i) ; 
-				//for (int j=0; j<8; j++) {
-					//red += (a * power(2, j)) ;
-				//}
-			//}
+			//System.out.println(sRGB);
+			for (int i=0; i<8; i++) {
+				int a =  (int) sRGB.charAt(i) -48 ; // pour passer du unicode a int
+				System.out.println(sRGB.charAt(i) + "   " + a) ;
+				for (int j=7; j>=0; j--) {
+					red += (a * power(2, j)) ;
+					System.out.print("     " + red + "     ");  // Problem, it does: (1*2^7 + 1*2^6 + ... + 1*2^0)  + (1*2^7 + 1*2^6 + ... + 1*2^0) + ... + (0*2^7 +...)
+				}
+			}
+		System.out.println(red) ;
 		return red; 
 	}
 		public static int power(int a, int b) {
