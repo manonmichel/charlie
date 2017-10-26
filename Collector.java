7f0a1f8386f663d5bef435249a6e08734a1eb575
Ellie <ellierabarison@gmail.com>
2017-10-27 05:53:44 +0800

    Update Collector.java

--- Collector.java
+++ Collector.java
@@ -10,11 +10,44 @@ public class Collector {
 	 * @param smallestFirst : a boolean, indicates if the smallest element is the best or not (biggest is then the best)
 	 * @return an array of two integer coordinates, row first and then column
 	 */
-	public static int[] findBest(double[][] matrix, boolean smallestFirst) {
-
-    	// TODO implement me !
-		return new int[]{};
+	
+	
+	
+	/** /!\ Les if dans des if me paraissent un peu dangereux though, il faut en discuter! En attendants je mets en mode annotation.
+	
+public static int[] findBest(double[][] matrix, boolean smallestFirst) {
+	
+	int[] coordonnees = new int[]{};
+	
+	double bestdistance = matrix[0][0];
+	
+	for (int i=0; i<matrix.length;i++) {
+		for (int j=0; j<matrix[i].length;j++) {
+					
+				if (smallestFirst) {
+					if (bestdistance < matrix[i][j]) {
+						bestdistance+=0;
+					}
+					else {
+						bestdistance=matrix[i][j];
+					}
+				}
+				
+				else {
+					if (bestdistance > matrix[i][j]) {
+						bestdistance+=0;
+					}
+					else {
+						bestdistance=matrix[i][j];
+					}
+				}
+				coordonnees[0]=i;
+				coordonnees[1]=j;
+		}
 	}
+	return coordonnees;
+} **/
+    	
 
 	
 	/**
