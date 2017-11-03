be9b7573e68cddfa705d1341d1d8c2769fc84173
Ellie <ellierabarison@gmail.com>
2017-11-03 21:01:52 +0800

    Update Program.java

--- Program.java
+++ Program.java
@@ -4,15 +4,24 @@ public class Program {
 	
 	public static void main(String[] args){
 		
-		String refBgCharlie = "images/charlie_beach.png";
-		String refPatternCharlie = "images/charlie.png";
-	
-	 	int[][] bgCharlie = Helper.read(refBgCharlie);
-	 	int[][] patternCharlie = Helper.read(refPatternCharlie);
+		System.out.println("Find Charlie");
+		
+	 	int[][] beach = Helper.read("images/charlie_beach.png");
+	 	int[][] charlie = Helper.read("images/charlie.png");
+	 	double[][] beachGray = ImageProcessing.toGray(beach);
+        double[][] charlieGray = ImageProcessing.toGray(charlie);
 	 	
-	 	double[][] distance = DistanceBasedSearch.distanceMatrix(patternCharlie, bgCharlie); 			
-	 	int[] p = Collector.findBest(distance, true);
-	 	Helper.drawBox(p[0], p[1], patternCharlie[0].length, patternCharlie.length, bgCharlie);
-	 	Helper.show(bgCharlie, "Found!");
+        // Search based on the Similarity between gray-scale images beachGray and charlieGray
+	 	System.out.println("Compute Similarity Matrix: expected time about 2 minutes.");
+	 	double[][] similarity = SimilarityBasedSearch.similarityMatrix(charlieGray, beachGray);
+	 	
+	 	// Looking for the coordinates of the window that resembles the pattern the most
+	 	System.out.println("Find Best");
+	 	int[] best = Collector.findBest(similarity, false);   
+    	
+	 	// Drawing a box around that window
+	 	Helper.drawBox(best[0], best[1], charlie[0].length, charlie.length, beach);
+	 	System.out.println("drawBox at (" + best[0] + "," + best[1] + ")");
+	 	Helper.show(beach, "Found!");
 	}	
-}
+} 
